645e74688aec6cc121673c6703dda1ff383b2f60
Angelika Kuleta <kuleta.an@gmail.com>
2021-05-20 12:24:49 +0200

    Create BasketController for Basket.API

---
+++ MicroMachines/Services/MicroMachines.Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,44 @@
+﻿using MicroMachines.Basket.API.Entities;
+using MicroMachines.Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace MicroServices.Basket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("userId:Guid", Name = "GetBasket")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ShoppingCart))]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(Guid userId)
+        {
+            var basket = await _repository.GetSingle(userId);
+            return Ok(basket ?? new ShoppingCart(userId));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ShoppingCart))]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart basket)
+        {
+            return Ok(await _repository.Edit(basket));
+        }
+
+        [HttpDelete("userId:Guid")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteBasket(Guid userId)
+        {
+            await _repository.Delete(userId);
+            return Ok();
+        }
+    }
+}
